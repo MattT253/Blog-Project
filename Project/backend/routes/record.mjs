bde3bf3cfad5e27d84b4de6293d40840460e9e8a
MattT253 <122591164+MattT253@users.noreply.github.com>
2025-03-17 08:51:21 +0000

    reworked some backend files

--- Project/backend/routes/record.mjs
+++ Project/backend/routes/record.mjs
@@ -4,16 +4,16 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// This section will help you get a list of all the records.
+// This section will help you get a list of all the posts.
 router.get("/", async (req, res) => {
-  let collection = await db.collection("records");
+  let collection = await db.collection("posts");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
-// This section will help you get a single record by id
+// This section will help you get a single post by id
 router.get("/:id", async (req, res) => {
-  let collection = await db.collection("records");
+  let collection = await db.collection("posts");
   let query = {_id: new ObjectId(req.params.id)};
   let result = await collection.findOne(query);
 
@@ -21,19 +21,19 @@ router.get("/:id", async (req, res) => {
   else res.send(result).status(200);
 });
 
-// This section will help you create a new record.
+// This section will help you create a new post.
 router.post("/", async (req, res) => {
   let newDocument = {
     name: req.body.name,
     position: req.body.position,
     level: req.body.level,
   };
-  let collection = await db.collection("records");
+  let collection = await db.collection("posts");
   let result = await collection.insertOne(newDocument);
   res.send(result).status(204);
 });
 
-// This section will help you update a record by id.
+// This section will help you update a user by id.
 router.patch("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
   const updates =  {
@@ -44,17 +44,17 @@ router.patch("/:id", async (req, res) => {
     }
   };
 
-  let collection = await db.collection("records");
+  let collection = await db.collection("posts");
   let result = await collection.updateOne(query, updates);
 
   res.send(result).status(200);
 });
 
-// This section will help you delete a record
+// This section will help you delete a post
 router.delete("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
 
-  const collection = db.collection("records");
+  const collection = db.collection("posts");
   let result = await collection.deleteOne(query);
 
   res.send(result).status(200);
